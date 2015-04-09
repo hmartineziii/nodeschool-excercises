2e0e405f439007719b2454255abd00718e39da3d
Hector Martinez III <hmartineziii@gmail.com>
2015-04-09 06:04:56 -0700

    ex12 half way done

--- learnyounode/ex12/program.js
+++ learnyounode/ex12/program.js
@@ -1,1 +1,16 @@
 var http = require('http');
+var map = require('through2-map')
+
+var server= http.createServer(function(req,res){
+	req.pipe(map(function (chunk){
+		console.log(chunk.toString())
+		// chunk.toString().toUpperCase()
+	}))
+	res.end()
+})
+server.listen(process.argv[2])
+// http.request({
+// 	method: POST
+// },function (req, res){
+// 	console.log(req)
+// })
