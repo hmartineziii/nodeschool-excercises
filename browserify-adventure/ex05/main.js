cff4ea317722f0c27c2c5859d7cb83a62f71e7c4
Hector Martinez III <hmartineziii@gmail.com>
2015-04-20 20:59:28 -0700

    ex05 changed complete

--- browserify-adventure/ex05/main.js
+++ browserify-adventure/ex05/main.js
@@ -3,5 +3,5 @@ var querystring = require('querystring');
 
 var addr = prompt();
 var query = url.parse(addr).query;
-
-console.log(url.resolve(addr, querystring.parse(query).file));
+var params = querystring.parse(query);
+console.log(url.resolve(addr, params.file));
