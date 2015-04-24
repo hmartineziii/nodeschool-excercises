c4e4851dae287cb75589fddd5e54ebca0eee1ae0
Hector Martinez III <hmartineziii@gmail.com>
2015-04-23 21:19:16 -0700

    finished ex06

--- browserify-adventure/ex06/widget.js
+++ browserify-adventure/ex06/widget.js
@@ -1,6 +1,17 @@
-var Widget = require('widget');
 var domify = require('domify');
+var html = '<div>Hello <span class="name"></span>!</div>';
 
-var w = Widget();
-w.setName('beep boop');
-w.appendTo(document.body);
+module.exports = Widget;
+
+function Widget () {
+  if (!(this instanceof Widget)) return new Widget;
+  this.element = domify(html);
+}
+
+Widget.prototype.setName = function (name) {
+  this.element.querySelector('.name').textContent = name;
+};
+
+Widget.prototype.appendTo = function (target) {
+  target.appendChild(this.element);
+};
