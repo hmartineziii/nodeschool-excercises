5201f5ede37e89673ef53ed5a3523e7bd4a58e49
Hector Martinez III <hmartineziii@gmail.com>
2015-04-27 19:43:16 -0700

    ex01 functional js completed

---
+++ browserify-adventure/ex08/main.js
@@ -0,0 +1,2 @@
+var txt = require('/usr/lib/node_modules/browserify-adventure/problems/writing_transforms/wake.txt');
+console.log(txt);
---
+++ browserify-adventure/ex08/tr.js
@@ -0,0 +1,22 @@
+var through = require('through2');
+var split = require('split');
+var sprintf = require('sprintf');
+var quote = require('quote-stream');
+var combine = require('stream-combiner2');
+
+module.exports = function (file) {
+    if (!/\.txt$/.test(file)) return through();
+    var num = 0;
+    var liner = through(function write (buf, enc, next) {
+        var line = buf.toString('utf8') + '\n';
+        if (num % 5 === 0) {
+            this.push(sprintf('%3d %s', num, line));
+        }
+        else this.push('    ' + line);
+        num ++;
+        next();
+    });
+    var prefix = through();
+    prefix.push('module.exports=');
+    return combine([ split(), liner, quote(), prefix ]);
+};
