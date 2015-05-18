03619df8001554bba47027a0a9feae868bc03288
Hector Martinez III <hmartineziii@gmail.com>
2015-05-17 18:43:51 -0700

    first attempt at reduce

--- functional-javascript-workshop/ex07/program.js
+++ functional-javascript-workshop/ex07/program.js
@@ -1,7 +1,10 @@
 function reduce(arr, fn, initial) {
   // SOLUTION GOES HERE
-
-
+  if (!arr.length) return []
+  var head = arr[0];
+  head = fn(arr[0]);
+  var tail = arr.slice(1);
+  return reduce(tail,fn,head))
 }
 
 module.exports = reduce
