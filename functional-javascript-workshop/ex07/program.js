e5ec985686eb7bfbc64379257289dabe7d271958
Hector Martinez III <hmartineziii@gmail.com>
2015-05-17 19:00:56 -0700

    further writing of reduce

--- functional-javascript-workshop/ex07/program.js
+++ functional-javascript-workshop/ex07/program.js
@@ -1,6 +1,6 @@
 function reduce(arr, fn, initial) {
   // SOLUTION GOES HERE
-  if (!arr.length) return []
+  if (!arr.length) return initial
   var head = arr[0];
   head = fn(arr[0]);
   var tail = arr.slice(1);
