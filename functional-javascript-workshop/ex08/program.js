d9e7ab50ac2919e488a5ef9815929425b309c0d3
Hector Martinez III <hmartineziii@gmail.com>
2015-05-18 18:29:38 -0700

    finished ex08 three different ways

---
+++ functional-javascript-workshop/ex08/program.js
@@ -0,0 +1,32 @@
+// function duckCount(){
+// 	var counter = 0
+// 	for ( i in duckCount.arguments){
+// 		if(Object.prototype.hasOwnProperty.call(duckCount.arguments[i], 'quack'))
+// 			counter++			
+// 	}
+// 	return counter
+// }
+
+
+
+//SOLUTION
+// function duckCount(){
+// 	return Array.prototype.slice.call(arguments).filter(function(obj){
+// 		return Object.prototype.hasOwnProperty.call(obj.'quack')
+// 	}).length
+// }
+
+//ALTERNATIVE SOLUTION
+function duckCount(){
+	return Array.prototype.slice.call(arguments).reduce(function(prev, curr, idx, array){
+		if (Object.prototype.hasOwnProperty.call(curr,'quack'))
+			return prev + 1
+		return prev
+	},0)
+}
+
+
+
+
+
+module.exports = duckCount
