c2bdc5501e5d214da7f857e3c27daf36f58e49b2
Hector Martinez III <hmartineziii@gmail.com>
2015-05-21 06:02:27 -0700

    ex03 makemehapi complete

---
+++ makemehapi/ex03/program.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+server.route({
+	path: '/',
+	method: 'GET',
+	handler: {
+		file: path.join(__dirname,'index.html')
+	}
+});
+
+server.start();
