f0831fc516c092fe211b54adcf95686eb9325e69
Hector Martinez III <hmartineziii@gmail.com>
2015-06-04 21:27:19 -0700

    done with ex05

--- lolololodash/ex05/program.js
+++ lolololodash/ex05/program.js
@@ -1,16 +1,14 @@
 var _ = require("lodash");
     
-var worker = function(collection) {
-    // do work; return stuff
-    var rObj = {};
-    var byPerson = _.groupBy(collection, 'username');
-    console.log(byPerson);
-    _.forEach(byPerson, function(n, key){
-    	console.log(n);
-    	rObj[key] = _.size(n);
-    })
-    return rObj
-};
-
-// export the worker function as a nodejs module
-module.exports = worker;
+    var worker = function(collection) {
+        // do work; return stuff
+        return _.chain(collection)
+        .map(function(value){
+        	return value.toUpperCase()+"CHAINED";
+        })
+        .sortBy()
+        .value();
+    };
+    
+    // export the worker function as a nodejs module
+    module.exports = worker;
