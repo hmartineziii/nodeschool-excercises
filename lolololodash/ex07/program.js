e2aa6adfbf53a1c5e09214ef766f95f84d23d3d7
Hector Martinez III <hmartineziii@gmail.com>
2015-06-08 22:25:53 -0700

    started ex07

---
+++ lolololodash/ex07/program.js
@@ -0,0 +1,8 @@
+var _ = require("lodash");
+    
+var worker = function(/* arguments */) {
+    // do work; return stuff
+};
+
+// export the worker function as a nodejs module
+module.exports = worker;
