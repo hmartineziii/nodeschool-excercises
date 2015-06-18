f80a725ead039a4596f17c62c613bf041310808d
Hector Martinez III <hmartineziii@gmail.com>
2015-06-18 06:27:55 -0700

    working through reduce

--- lolololodash/ex07/program.js
+++ lolololodash/ex07/program.js
@@ -1,7 +1,11 @@
 var _ = require("lodash");
     
-var worker = function(/* arguments */) {
+var worker = function(collection) {
     // do work; return stuff
+    _.reduce(collection, function(result, current, key, collection){
+    	// if (result[current].article)
+    		// result[current].quantity = 
+    },{})
 };
 
 // export the worker function as a nodejs module
