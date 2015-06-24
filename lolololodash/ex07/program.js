a496b7e71a105eccc8a4e7da60d887592aebf826
Hector Martinez III <hmartineziii@gmail.com>
2015-06-23 20:25:39 -0700

    ex07 more

--- lolololodash/ex07/program.js
+++ lolololodash/ex07/program.js
@@ -3,12 +3,13 @@ var _ = require("lodash");
 var worker = function(collection) {
     // do work; return stuff
     var orders = _.groupBy(collection, 'article');
-
+    var arr =[];
     var result = _.forEach(orders, function(item){
     	return _.reduce(item,function(person){
-
+    		return person.total_orders
     	},{})
     });
+    return result
 };
 
 // export the worker function as a nodejs module
