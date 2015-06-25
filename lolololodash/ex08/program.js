55944bff5d36f2893964f513bec4dd34dd8a36c1
Hector Martinez III <hmartineziii@gmail.com>
2015-06-24 20:44:36 -0700

    created template for ex08

---
+++ lolololodash/ex08/program.js
@@ -0,0 +1,9 @@
+// include the Lo-Dash library
+var _ = require("lodash");
+
+var worker = function(/* arguments */) {
+    // do work; return stuff
+};
+
+// export the worker function as a nodejs module
+module.exports = worker;
