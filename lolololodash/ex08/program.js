44747a556891e2ad1635b6d9d3f66a398fa92aa3
Hector Martinez III <hmartineziii@gmail.com>
2015-07-01 21:14:18 -0700

    done with ex08

--- lolololodash/ex08/program.js
+++ lolololodash/ex08/program.js
@@ -1,8 +1,25 @@
 // include the Lo-Dash library
 var _ = require("lodash");
 
-var worker = function(/* arguments */) {
+var worker = function(collection) {
     // do work; return stuff
+    var sum = _.reduce(collection, function(total,n){
+    	return total + n.income
+    },0);
+    var avg = sum / collection.length;
+    // console.log(avg);
+
+    var underperform = _.sortBy(_.filter(collection, function(entry){
+    	return entry.income <= avg
+    }),'income');
+    // console.log(underperform);
+    var overperform = _.sortBy(_.filter(collection, function(entry){
+    	return entry.income > avg
+    }),'income');
+
+    return {"average": avg,
+			"underperform": underperform,
+			"overperform": overperform}
 };
 
 // export the worker function as a nodejs module
