bf1e7c86cbfc1eb7d9fe5d83008bfeff87d92139
Hector Martinez III <hmartineziii@gmail.com>
2015-07-30 21:40:01 -0700

    done with promises ex01

--- promise-it-wont-hurt/ex01/program.js
+++ promise-it-wont-hurt/ex01/program.js
@@ -1,5 +1,17 @@
 var q = require('q');
 var defer = q.defer(); 
-//defer.promise is the actual promise itself
-//defer.promise.then is the "Q" way of attaching a then handler
-//your solution here 
+
+defer.promise.then(console.log);
+
+setTimeout(function(){
+	defer.resolve("RESOLVED!")	
+},300);
+
+
+
+// //solution
+// var q = require('q');
+// var def = q.defer();
+
+// def.promise.then(console.log);
+// setTimeout(def.resolve, 300, "RESOLVED!");
